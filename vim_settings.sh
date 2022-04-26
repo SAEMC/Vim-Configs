df3e4e79b0e816f71f8d99d7259d55f64915a28a
SAEMC <saemc27@gmail.com>
2022-04-27 03:37:47 +0900

    Mod vim_settings.sh

--- vim_settings.sh
+++ vim_settings.sh
@@ -77,12 +77,12 @@ vim +PluginInstall +qall
 cat >>${HOME}/.vimrc <<EOF
 
 if (empty(\$TMUX))
-  if (has("nvim"))
-    let \$NVIM_TUI_ENABLE_TRUE_COLOR=1
-  endif
-  if (has("termguicolors"))
-    set termguicolors
-  endif
+    if (has("nvim"))
+        let \$NVIM_TUI_ENABLE_TRUE_COLOR=1
+    endif
+    if (has("termguicolors"))
+        set termguicolors
+    endif
 endif
 
 set expandtab
