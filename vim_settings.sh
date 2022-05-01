d86ae1eed990d2a47bbf1309343a3b7589baaa5c
SAEMC <saemc27@gmail.com>
2022-05-01 19:59:54 +0900

    Mod vim_settings.sh

--- vim_settings.sh
+++ vim_settings.sh
@@ -78,8 +78,10 @@ EOF
 
 # NVM
 export NVM_DIR="\$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+# This loads nvm
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+# This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
 EOF
     fi
 
