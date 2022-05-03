465fd4ca957c254aa81e6f620847ef08b834a527
SAEMC <saemc27@gmail.com>
2022-05-03 16:36:21 +0900

    Mod vim_settings.sh

--- vim_settings.sh
+++ vim_settings.sh
@@ -43,7 +43,7 @@ EOF
     eval "$node_check"
     if [[ "$?" -ne 0 ]]; then
         (su - $USER -c 'export NVM_DIR=/usr/local/nvm && source /opt/nvm/nvm.sh && \
-            nvm install --lts && nvm alias default lts/* && nvm use lts/*')
+            sudo nvm install --lts && sudo nvm alias default lts/* && sudo nvm use lts/*')
     fi
 
     sudo apt-get update && sudo apt-get install -y python3-venv universal-ctags
