9127527b30bd1f2feb5cd2cdec69bff0c31bc81a
SAEMC <saemc27@gmail.com>
2022-05-25 17:20:55 +0900

    Mod vim_settings.sh

--- vim_settings.sh
+++ vim_settings.sh
@@ -497,7 +497,7 @@ let g:coc_global_extensions = [
 " [[ Functions ]]
 function! EnterSelect()
     if pumvisible() && complete_info()["selected"] == -1
-        return "\<C-y>\<CR>"
+        return "\<C-y>"
     elseif pumvisible()
         return coc#_select_confirm()
     else
