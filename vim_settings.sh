039006a320c9357b83dd3f559293ee80f7dfd15a
SAEMC <saemc27@gmail.com>
2022-05-27 15:16:11 +0900

    Mod vim_settings.sh

--- vim_settings.sh
+++ vim_settings.sh
@@ -543,13 +543,13 @@ vnoremap <silent> <Leader>m c
 vnoremap <silent> <Leader>s :<C-u>call SurroundCode()<CR>
 vnoremap <silent> <Leader>y :<C-u>OSCYank<CR>
 
-inoremap <silent> <expr> <CR> EnterSelect()
-inoremap <silent> <expr> <TAB> pumvisible() ? "\<C-n>" : col('.') < col('$') ? "\<Right>" : "\<Tab>"
 inoremap <silent> <expr> <C-d> col('.') < col('$') ? "\<C-o>x" : "\<Right>"
 inoremap <silent> <expr> <C-j> coc#float#has_scroll() ? "\<C-r>=coc#float#scroll(1, 1)\<CR>" : "\<C-o>j"
 inoremap <silent> <expr> <C-k> coc#float#has_scroll() ? "\<C-r>=coc#float#scroll(0, 1)\<CR>" : "\<C-o>k"
 inoremap <silent> <expr> <C-Space> coc#refresh()
 inoremap <silent> <expr> <S-TAB> pumvisible() ? "\<C-p>" : "\<Left>"
+inoremap <silent> <expr> <CR> EnterSelect()
+inoremap <silent> <expr> <TAB> pumvisible() ? "\<C-n>" : col('.') < col('$') ? "\<Right>" : "\<Tab>"
 inoremap <silent> <C-h> <C-o>h
 inoremap <silent> <C-l> <C-o>l
 
