337f33cffd7449f0d549b617513bca85ff16c0c8
SAEMC <saemc27@gmail.com>
2022-07-06 15:35:12 +0900

    Update

--- vim_settings.sh
+++ vim_settings.sh
@@ -473,6 +473,12 @@ let g:oscyank_max_length = 1000000
 let g:oscyank_silent = v:true
 let g:oscyank_term = 'tmux'
 let g:rainbow_active = 1
+let g:slime_default_config = {
+\ 'socket_name': get(split(\$TMUX, ','), 0),
+\ 'target_pane': '{top-right}' }
+let g:slime_dont_ask_default = 1
+let g:slime_python_ipython = 1
+let g:slime_target = 'tmux'
 let g:surround_no_mappings = 1
 let g:tagbar_autoclose = 0
 let g:tagbar_autofocus = 1
